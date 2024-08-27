2db77decece2881dd4b4255326cc694e8fbc4bb8
devingast <devingast@protonmail.com>
2024-08-26 20:53:41 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-// We are setting up this variable while making it be filled with the users input
-let Fname = prompt("Please type your first name in:");
+for (let count = 0; count <= 10; count++) { // THis is a loop that will essentially count from 0-10
 
-// Say hello to the person through another popup
-alert("Hello, " + Fname + "!");
+  if (count % 2 === 0) { // This will check if the current number in the loop is even or odd
+    document.write("Count " + count + " is even<br>"); // display that the count is even, line break
+  } else { // UNLESS IF
+    document.write("Count " + count + " is odd<br>"); // if odd, display that the number is odd
+  }
+} // conditions and loop
 
-// We are creating a constant variable for the value of pi here
-const piValue = 3.1415926;
+let myNum = prompt(" Please enter a number between 5 and 20:"); // ask the user for a number between 5 - 20 and store it.
 
-// Creating a variable while putting its value as the users input
-let myFavNum = prompt("What is your favorite number?");
+let counter = 1; // We are using a Do While loop here so the loop counters will equal the variable myNum
+do {
+  document.write("Loop counter: " + counter + "<br>"); // Kind of self explanitory
+  counter++;
+} while (counter <= myNum); // do the loop counter until counter is equal to the number stored in myNum
 
-// Calculate the area of a circle with users entered number and pi value.
-let myArea = myFavNum * myFavNum * piValue;
+let subjects = ["Accounting", "Algebra", "Programming", "Art", "Data Analytics"]; // setting up an array for the subjects
 
-// We are displaying the results below on the webapge and in the console.
-document.write("Hello, " + Fname + "<br>");
-document.write("Your favorite number is: " + myFavNum + "<br>");
-document.write("If that was the radius of a circle, the circle's area would be: " + myArea);
-console.log("Hello, " + Fname);
-console.log("Your favourite number is: " + myFavNum);
-console.log("If that was the radius of a circle, the circle's area would be: " + myArea);
+subjects.forEach(function(subject) { // We are displaying the array values in order using foreach()
+  document.write("Subject: " + subject + "<br>"); // displaying everything
+});
+
+document.write("Subjects: " + subjects.join(", ")); //diisplay the array values separated by commas!
